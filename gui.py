7213dc3387e4d98962354075163293dcda490bc6
Bhargav Manavadariya <bhargavmanavadariya1998@gmail.com>
2023-05-20 17:39:26 -0400

    implementation of "Edit todo " button and do interaction with code base and also done changes for run time visible changes

--- gui.py
+++ gui.py
@@ -3,8 +3,12 @@ import PySimpleGUI as sg
 
 label = sg.Text('Enter a todo')
 Input_box = sg.InputText(tooltip="enter a todo", key='todo')
-button = sg.Button("ADD")
-window = sg.Window('My Todo App', layout=[[label], [Input_box, button]])
+button1 = sg.Button("ADD")
+list_box = sg.Listbox(values=functions.get_todos(), key="todos",
+                      enable_events=True, size=[45,10])
+button2 = sg.Button("EDIT")
+window = sg.Window('My Todo App', layout=[[label], [Input_box, button1],
+                                          [list_box, button2]])
 while 1:
     value, item = window.read()
     print(value)
@@ -15,6 +19,17 @@ while 1:
             todos = functions.get_todos()
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "EDIT":
+            new_todo = item['todo']
+            todos_to_edit = item['todos'][0]
+            todos = functions.get_todos()
+            i = todos.index(todos_to_edit)
+            todos[i] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "todos":
+            window['todo'].update(value=item['todos'][0])
         case sg.WINDOW_CLOSED:
             break
 window.close()
